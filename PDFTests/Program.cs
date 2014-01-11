b93cca269136cf2a579a5cd01a7b61bbc5ac53ea
Gordon Watts <gordon@gordonandpaula.net>
2014-01-10 22:52:58 -0800

    First import

---
+++ PDFTests/Program.cs
@@ -0,0 +1,27 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Linq;
+
+namespace PDFTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = PdfReader.Open("testdoc.pdf");
+
+            Console.WriteLine("Found {0} pages.", doc.PageCount);
+            var font = new XFont("Verdana", 20, XFontStyle.Bold);
+
+            foreach (var page in doc.Pages.Cast<PdfPage>())
+            {
+                var gfc = XGraphics.FromPdfPage(page);
+                gfc.DrawString("Hello World!", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            }
+
+            doc.Save("output.pdf");
+        }
+    }
+}
